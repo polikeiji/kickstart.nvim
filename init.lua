cb2abfdabdfad5257ec82aa6242dc421801e1fdf
keiji <khokamura@microsoft.com>
2024-04-25 14:08:43 +0900

    config: configurations for terminal

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -1028,9 +1028,9 @@ require('lazy').setup({
 -- [[ My custom configurations ]]
 
 -- Disable line number in the terminal mode
-vim.api.nvim_create_autocmd({"TermOpen"}, {
-  pattern = {"*"},
-  command = "setlocal nonumber norelativenumber",
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+  pattern = { '*' },
+  command = 'setlocal nonumber norelativenumber',
 })
 
 -- Enable paste in the terminal mode
@@ -1038,7 +1038,7 @@ vim.api.nvim_create_autocmd({"TermOpen"}, {
 vim.keymap.set('t', '<c-r>', function()
   local next_char_code = vim.fn.getchar()
   local next_char = vim.fn.nr2char(next_char_code)
-  return '<C-\\><C-N>"'..next_char..'pi'
+  return '<C-\\><C-N>"' .. next_char .. 'pi'
 end, { expr = true })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
